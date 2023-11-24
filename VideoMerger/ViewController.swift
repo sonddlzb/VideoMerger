8bab5a49cd7fca7fb7d4c3b2f1fd622c246fa2fe
datnx <datnx@yopaz.vn>
2023-11-24 11:39:25 +0700

    Initial Commit

---
+++ VideoMerger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoMerger
+//
+//  Created by đào sơn on 24/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
